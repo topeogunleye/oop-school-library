9fd181331a3c7179a7276c70b9057eb40ac94974
Temitope Ogunleye <topeogunleye1@gmail.com>
2022-03-14 11:04:17 +0100

    Add classroom class and its methods

---
+++ classroom.rb
@@ -0,0 +1,17 @@
+require './student'
+require './person'
+
+class Classroom
+  attr_accessor :students
+
+  def initialize(label)
+    @label = label
+    @students = []
+  end
+
+  def add_student(student)
+    @students.push(student)
+    student.classroom = self
+  end
+end
+
