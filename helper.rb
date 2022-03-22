8c35cdfcaa6351cab8f36ba1cc759b712f70f058
Temitope Ogunleye <topeogunleye1@gmail.com>
2022-03-22 18:23:38 +0100

    Update helper file

--- helper.rb
+++ helper.rb
@@ -1,38 +1,57 @@
+require './menu_operations'
+
 # helpers
-def user_info
-  print 'Enter the name of the person: '
-  name = gets.chomp
-  print 'Age: '
-  age = gets.chomp
-  [name, age]
-end
-
-def create_student
-  name, age = user_info
-  print 'Has parent permission? [Y/N]: '
-  parent_permission = gets.chomp
-  print 'Enter the classroom: '
-  classroom = gets.chomp
-  case parent_permission.upcase
-  when 'Y'
-    permission = true
-  when 'N'
-    permission = false
-  else
-    puts 'Invalid input'
-    run
+class Helper
+  def self.user_info
+    print 'Enter the name of the person: '
+    name = gets.chomp
+    print 'Age: '
+    age = gets.chomp
+    [name, age]
   end
-  student = Student.new(classroom, age, name, parent_permission: permission)
-  people_store(student)
-  success('Student')
-end
 
-def create_teacher
-  # destructure name and age from user info
-  name, age = user_info
-  puts 'Enter the specialization: '
-  specialization = gets.chomp
-  teacher = Teacher.new(specialization, age, name)
-  people_store(teacher)
-  success('Teacher')
+  def self.create_student
+    name, age = self.user_info
+    print 'Has parent permission? [Y/N]: '
+    parent_permission = gets.chomp
+    print 'Enter the classroom: '
+    classroom = gets.chomp
+    case parent_permission.upcase
+    when 'Y'
+      permission = true
+    when 'N'
+      permission = false
+    else
+      puts 'Invalid input'
+      run
+    end
+    student = Student.new(classroom, age, name, parent_permission: permission)
+    student.parent_permission = permission
+    Menu_Operations.push(student)
+    success('Student')
+  end
+
+  def self.create_teacher
+    # destructure name and age from user info
+    name, age = self.user_info
+    puts 'Enter the specialization: '
+    specialization = gets.chomp
+    teacher = Teacher.new(specialization, age, name)
+    teacher.specialization = specialization
+    Menu_Operations.push(teacher)
+    success('Teacher')
+  end
+
+  def self.create_file_if_not_exist(file)
+    File.open(file, 'w') unless File.exist?(file)
+    File.write(file, '[]') 
+  end
+
+  def self.read_convert(filename)
+    JSON.parse(File.read(filename))
+  end
+
+  def self.success(message)
+    puts "Successfully created #{message}"
+  end
 end
