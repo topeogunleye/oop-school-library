f71f5b1c79433fed816ece3cc7c42297829e5219
Temitope Ogunleye <topeogunleye1@gmail.com>
2022-03-23 11:58:41 +0100

    Fix a linter error

--- corrector.rb
+++ corrector.rb
@@ -1,7 +1,7 @@
 class Corrector
   def correct_name(name)
     correct_name = name.capitalize
-    correct name = name.correct_name[0..9]
+    correct_name.correct_name[0..9]
     correct_name
   end
 end
